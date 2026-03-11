583977a3f2a122458ce8a91ae17a62146f081267
auto_jenkins_wlan5 <auto_jenkins_wlan5@realsil.com.cn>
2026-03-11 11:08:45 +0800

    update sdk
    ameba-tflite-micro version: 457e6b7
    1. [common][tflm] Upgrade CMSIS-NN: performance optimization

--- cmsis-nn.cmake
+++ cmsis-nn.cmake
@@ -2,8 +2,8 @@ include(FetchContent)
 
 set(cmsis_nn_dir ${download_dir}/cmsis_nn)
 set(cmsis_nn_zip ${download_dir}/cmsis_nn.zip)
-set(ZIP_PREFIX_NN "e096196a0c49f065abc03d943c583cd50de424ba")
-set(CMSIS_NN_MD5 "d2a6bc4330fed5653c74fc5dae31fd3a")
+set(ZIP_PREFIX_NN "88f1982a69c00ed13dd633a63da1009c48abbb4d")
+set(CMSIS_NN_MD5 "703bf3b2508a697aaf81f1a235788ceb")
 
 FetchContent_Declare(
   cmsis_nn
@@ -16,9 +16,10 @@ FetchContent_Declare(
 FetchContent_MakeAvailable(cmsis_nn)
 get_target_property(TARGET_COMPILE_OPTIONS cmsis-nn COMPILE_OPTIONS)
 if (TARGET_COMPILE_OPTIONS)
-    # The compiler option *'-fno-builtin'* does not utilize optimized implementations of e.g. memcpy and memset, 
+    # The compiler option *'-fno-builtin'* does not utilize optimized implementations of e.g. memcpy and memset,
     # which are heavily used by CMSIS-NN
     list(REMOVE_ITEM TARGET_COMPILE_OPTIONS "-fno-builtin" "-fno-builtin-memcpy" "-fno-builtin-memset")
     list(APPEND TARGET_COMPILE_OPTIONS "-Wno-undef")
+    list(APPEND TARGET_COMPILE_OPTIONS "-DCMSIS_NN_USE_REQUANTIZE_INLINE_ASSEMBLY")
     set_target_properties(cmsis-nn PROPERTIES COMPILE_OPTIONS "${TARGET_COMPILE_OPTIONS}")
 endif()
