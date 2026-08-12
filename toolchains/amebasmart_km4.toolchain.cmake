729bd9c221a847f7c3b7a31d1907bf6f3e5261a9
auto_jenkins_wlan5 <auto_jenkins_wlan5@realsil.com.cn>
2026-08-13 01:39:17 +0800

    update sdk
    ameba-tflite-micro version: 4a008f2
    1. [common][tflm] fix build error in cmake version < 3.24
    2. [common][tflm] add persistent/non-persistent arena usage query APIs and preserve local modifications on sync
    3. [common][tflie-micro] support with CMSIS-NN MVE
    4. [common][tflie-micro] add CMSIS-NN Sub kernel optimization and benchmark

--- toolchains/amebasmart_km4.toolchain.cmake
+++ toolchains/amebasmart_km4.toolchain.cmake
@@ -1,8 +1,11 @@
+set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
+
 # Define the toolchain directory and SDK toolchain path
 set(ToolChainVerMajor asdk-10.3.1)
 set(ToolChainVerMinor 4602)
 set(TOOLCHAIN_DIR /opt/rtk-toolchain/${ToolChainVerMajor}-${ToolChainVerMinor})
 set(SDK_TOOLCHAIN ${TOOLCHAIN_DIR}/linux/newlib)
+
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
@@ -13,10 +16,8 @@ set(CMAKE_CXX_COMPILER  ${SDK_TOOLCHAIN}/bin/${TOOL_PREFIX}g++)
 set(CMAKE_RANLIB        ${SDK_TOOLCHAIN}/bin/${TOOL_PREFIX}ranlib)
 set(CMAKE_AR            ${SDK_TOOLCHAIN}/bin/${TOOL_PREFIX}ar)
 
-set(ARCH_FLAGS "-march=armv8.1-m.main+dsp -mthumb -mcmse -mfloat-abi=hard -mfpu=fpv5-sp-d16 -D__FPU_PRESENT \
-    -flax-vector-conversions -fno-tree-loop-distribute-patterns -lc -lrdimon")
-string(APPEND ARCH_FLAGS " -O2 ")
-
+# AMEBASMART (RTL8730E) supports MVE (Helium) extension
+set(ARCH_FLAGS          "-O2 -march=armv8.1-m.main+mve -mthumb -mcmse -mfloat-abi=hard -mfpu=fpv5-sp-d16 -D__FPU_PRESENT=1 -flax-vector-conversions -fno-tree-loop-distribute-patterns")
 set(CMAKE_C_FLAGS       "${ARCH_FLAGS}" CACHE STRING "" FORCE )
 set(CMAKE_CXX_FLAGS     "${ARCH_FLAGS}" CACHE STRING "" FORCE )
 set(CMAKE_ASM_FLAGS     ${CMAKE_C_FLAGS} CACHE STRING "" FORCE )
@@ -31,4 +32,3 @@ set(CMAKE_C_ARCHIVE_CREATE "<CMAKE_AR> qcD <TARGET> <LINK_FLAGS> <OBJECTS>")
 set(CMAKE_C_ARCHIVE_FINISH "<CMAKE_RANLIB> -D <TARGET>")
 set(CMAKE_CXX_ARCHIVE_CREATE "<CMAKE_AR> qcD <TARGET> <LINK_FLAGS> <OBJECTS>")
 set(CMAKE_CXX_ARCHIVE_FINISH "<CMAKE_RANLIB> -D <TARGET>")
-
